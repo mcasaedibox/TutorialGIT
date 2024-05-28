492547438361c0cc6353cfd60e8962f091ffec9b
Michael Casa <casa.m.michael@gmail.com>
2024-05-28 13:38:14 +0200

    Agregada clase USUARIO.

---
+++ Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace TutorialGIT.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
