feb7848e85fdfbeb462c2a3757ed8213a545cb89
Michael Casa <casa.m.michael@gmail.com>
2024-05-28 13:40:49 +0200

    Agregado APELLIDO a clase usuario. Estamos en rama MASTER.

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-
+        public string Apellido { get; set; }
     }
 }
